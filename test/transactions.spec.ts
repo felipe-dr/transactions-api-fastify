17328ddad49239fb47d5211339347ff967d1f6a6
Felipe DR <felipe.corp7@gmail.com>
2025-05-25 15:55:04 -0300

    feat: class-04 setting up test data

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,6 @@
-import { describe } from 'node:test'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
-import { afterAll, beforeAll, expect, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 
 import { app } from '../src/app'
 
@@ -11,6 +11,11 @@ describe('Transactions routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
